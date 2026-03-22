65f75ad4fa0366dee9f32af84576b74c5bf8e630
Brandon Boch López <alejandrobochlopez@gmail.com>
2026-03-22 15:28:47 -0600

    Agrego capturas al portafolio

--- src/App.js
+++ src/App.js
@@ -15,11 +15,30 @@ function App() {
       <div>
         <h4>💸 App de Control de Gastos</h4>
         <p>Aplicación para gestionar gastos personales con dashboard de indicadores.</p>
+          <a href="https://github.com/alejandroboch/app-control-de-gastos" target="_blank">
+            Ver código
+          </a>
+          <img src="/screenshots/aplicacion_gastos/login_screen.jpeg" width="300" />
+          <img src="/screenshots/aplicacion_gastos/add_expense_screen.jpeg" width="300" />
+          <img src="/screenshots/aplicacion_gastos/create_categories_screen.jpeg" width="300" />
+          <img src="/screenshots/aplicacion_gastos/expense_history_screen.jpeg" width="304" />
+          <img src="/screenshots/aplicacion_gastos/dashboard_screen.jpeg" width="204" />
       </div>
 
       <div>
         <h4>🛒 App de Abarrotería</h4>
         <p>Aplicación para control de inventario y ventas en una tienda.</p>
+        <a href="https://github.com/alejandroboch/app-abarroteria-miscelania" target="_blank">
+            Ver código
+          </a>
+          <img src="/screenshots/aplicacion_abarroteria/pantalla_login.jpeg" width="300" />
+          <img src="/screenshots/aplicacion_abarroteria/pantalla_principal.jpeg" width="300" />
+          <img src="/screenshots/aplicacion_abarroteria/pantalla_registro_usuario.jpeg" width="266" />
+          <img src="/screenshots/aplicacion_abarroteria/pantalla_registro_productos.jpeg" width="300" />
+          <img src="/screenshots/aplicacion_abarroteria/pantalla_historial_ventas.jpeg" width="300" />
+          <img src="/screenshots/aplicacion_abarroteria/pantalla_inventario.jpeg" width="238.9" />
+          <img src="/screenshots/aplicacion_abarroteria/pantalla_reportes.jpeg" width="300" />
+          <img src="/screenshots/aplicacion_abarroteria/pantalla_notificaciones.jpeg" width="300" />
       </div>
 
       <h3>Contacto</h3>
