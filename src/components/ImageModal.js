c5fa005d5cd4fdca8a0fe553410402dff8f08f0a
Brandon Boch López <alejandrobochlopez@gmail.com>
2026-03-22 20:06:21 -0600

    Mejoras para visualizar en movil 2

--- src/components/ImageModal.js
+++ src/components/ImageModal.js
@@ -72,23 +72,20 @@ const nextImage = () => {
 
 
   return (
-    <div className="modal-overlay" onClick={onClose}>
-        <button className="close-btn" onClick={onClose}>✖</button>
+   <div className="modal-overlay" onClick={onClose}>
+  <button className="close-btn" onClick={onClose}>✖</button>
 
-        <div
-            className="modal-content"
-            onClick={(e) => e.stopPropagation()}
-            onTouchStart={handleTouchStart}
-            onTouchMove={handleTouchMove}
-            onTouchEnd={handleTouchEnd}
-        >
-    {/* Flecha izquierda */}
-            <button className="nav-btn left" onClick={prevImage}>⬅</button>
-            <img src={current.src} alt={current.alt} />
-            <p>{current.label}</p>
-
-    {/* Flecha derecha */}
-            <button className="nav-btn right" onClick={nextImage}>➡</button>
+  <div
+    className="modal-content"
+    onClick={(e) => e.stopPropagation()}
+    onTouchStart={handleTouchStart}
+    onTouchMove={handleTouchMove}
+    onTouchEnd={handleTouchEnd}
+  >
+    <button className="nav-btn left" onClick={prevImage}>⬅</button>
+    <img src={current.src} alt={current.alt} />
+    <p>{current.label}</p>
+    <button className="nav-btn right" onClick={nextImage}>➡</button>
   </div>
 </div>
   );
